d5111be26045fcc9aeea8bd9006d11abc53b562b
SASITHAR M <153161335+codesasithar@users.noreply.github.com>
2025-07-20 07:38:15 +0530

    Update CursorEffects.tsx

--- src/components/CursorEffects.tsx
+++ src/components/CursorEffects.tsx
@@ -152,7 +152,7 @@ const CursorEffects = () => {
           style={{
             left: trail.x,
             top: trail.y,
-            opacity: (index + 1) / trails.length * 0.5,
+            opacity: (index + 1) / trails.length * 0.9,
             transform: `scale(${(index + 1) / trails.length})`,
           }}
         />
