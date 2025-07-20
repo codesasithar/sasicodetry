e748bce410dcbcd6756d208e0922364c3ad3077e
SASITHAR M <153161335+codesasithar@users.noreply.github.com>
2025-07-20 07:45:12 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -208,7 +208,7 @@ const Contact = () => {
                 ) : (
                   <>
                     Ping Sasi
-                    <Send/>
+                    
                   </>
                 )}
               </button>
